fb76d4e1cf3e2252645353c0826b199d5f50e646
anastasia.luntovskaya <anastasia.luntovskaya@gmail.com>
2022-07-20 18:20:08 +0300

    fix mercuryCashOut & mercuryCloseSession order

--- addons/point_of_sale/static/src/js/Popups/ClosePosPopup.js
+++ addons/point_of_sale/static/src/js/Popups/ClosePosPopup.js
@@ -144,8 +144,8 @@ odoo.define('point_of_sale.ClosePosPopup', function(require) {
                     }
                 }
                 try {
-                    await mercuryCloseSession(false);
                     await mercuryCashOut(this.state.payments[this.defaultCashDetails.id].counted, false);
+                    await mercuryCloseSession(false);
                 } catch (error) {
                      return await this.showPopup('ErrorPopup', mercuryCreateErrorPopupBody(error));
                 }
